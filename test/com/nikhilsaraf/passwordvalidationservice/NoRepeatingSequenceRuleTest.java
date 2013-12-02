e13b56e8dbf0ff0f69042db8475cbd74054a62f5
Nikhil <nikhilsaraf89@gmail.com>
2013-12-02 01:54:40 -0800

    Added some more tests for repeating rule. all passing now!

--- test/com/nikhilsaraf/passwordvalidationservice/NoRepeatingSequenceRuleTest.java
+++ test/com/nikhilsaraf/passwordvalidationservice/NoRepeatingSequenceRuleTest.java
@@ -22,9 +22,25 @@ public class NoRepeatingSequenceRuleTest extends BaseRuleTest {
 	 * @throws Exception
 	 */
 	@Test
-	public void testRepeatedSubsequenceRejected() throws Exception {
+	public void testRepeatedSubsequenceRejected1() throws Exception {
 		commonTestHelper("ABCABC", NoRepeatingSequenceRule.constructErrorMessage(3, 5));
 	}
+	
+	@Test
+	public void testRepeatedSubsequenceRejected2() throws Exception {
+		commonTestHelper("ABCDABABC", NoRepeatingSequenceRule.constructErrorMessage(6, 7));
+	}
+	
+	@Test
+	public void testRepeatedSubsequenceRejected3() throws Exception {
+		commonTestHelper("THISWILLREPEAT", NoRepeatingSequenceRule.constructErrorMessage(7, 7));
+	}
+	
+	@Test
+	public void testRepeatedSubsequenceRejected4() throws Exception {
+		commonTestHelper("THISDIDREPEATHISDIDREPEAT", NoRepeatingSequenceRule.constructErrorMessage(12, 23));
+	}
+	
 	
 	/**
 	 * Note how uppercase is ok, because this rule does not define any character restraints but only repeating
@@ -33,7 +49,17 @@ public class NoRepeatingSequenceRuleTest extends BaseRuleTest {
 	 * @throws Exception
 	 */
 	@Test
-	public void testNonRepeatedSubsequenceAccepted() throws Exception {
+	public void testNonRepeatedSubsequenceAccepted1() throws Exception {
 		commonTestHelper("ABCAB", null);
+	}
+	
+	@Test
+	public void testNonRepeatedSubsequenceAccepted2() throws Exception {
+		commonTestHelper("ABCABD", null);
+	}
+	
+	@Test
+	public void testNonRepeatedSubsequenceAccepted3() throws Exception {
+		commonTestHelper("ABCDABC", null);
 	}
 }
